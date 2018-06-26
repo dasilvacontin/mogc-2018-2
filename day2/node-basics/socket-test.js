c5742620558ca8b5c4239c352fed4747c872e180
David da Silva <dasilvacontin@gmail.com>
2018-06-26 17:12:06 +0200

    initial socket test

---
+++ day2/node-basics/socket-test.js
@@ -0,0 +1,22 @@
+// Setup basic express server
+var express = require('express')
+var app = express()
+var path = require('path')
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
+var port = process.env.PORT || 3000
+
+// app.get('/socket.io/socket.io.js', function () {
+//     // devolver libreria socket io cliente
+// })
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port)
+})
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', (socket) => {
+
+})
